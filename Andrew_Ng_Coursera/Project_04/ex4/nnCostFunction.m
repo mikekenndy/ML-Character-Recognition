59fc45f088a077f480662e9d0dfa8e3792cbeabf
mikekenndy <Mike.Kennedy.Dev@gmail.com>
2019-09-14 16:32:00 -0400

    Updated comments

--- Andrew_Ng_Coursera/Project_04/ex4/nnCostFunction.m
+++ Andrew_Ng_Coursera/Project_04/ex4/nnCostFunction.m
@@ -123,13 +123,15 @@ J += (lambda/(2*m)) * (reg1 + reg2);
 % ----- Part 2 ----- %
 % Backpropagation
 
+% Calculate error of output
 err_out = hx - Y;
-
-err_hdn = err_out * Theta2(:,2:end) .* sigmoidGradient(Z2);
-
 delta_1 = err_hdn' * A1;
+
+% Calculate error to hidden layer
+err_hdn = err_out * Theta2(:,2:end) .* sigmoidGradient(Z2);
 delta_2 = err_out' * A2;
 
+% Calculate gradient for Theta 1 and 2
 Theta1_grad = delta_1 ./ m;
 Theta2_grad = delta_2 ./ m;
 
